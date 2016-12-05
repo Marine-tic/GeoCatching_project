086567643db9b2d2324c7135203894f657d3e1b8
Marine LAndraudie <mlandraudie@mobibase.com>
2016-12-05 16:23:24 +0100

    Ajout des fichiers du projet Android

---
+++ Geocatching/app/src/main/java/iut/unice/fr/geocatching/MainActivity.java
@@ -0,0 +1,13 @@
+package iut.unice.fr.geocatching;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
