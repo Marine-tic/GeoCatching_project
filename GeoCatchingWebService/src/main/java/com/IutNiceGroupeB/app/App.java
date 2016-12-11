ed5376b4febc37ba4fb3da75880b39464230a9e7
Jeremy <jeremy.paroux@gmail.com>
2016-12-11 23:07:26 +0100

    Dézippage de geoCatchingWebService et mise à jour du readme

--- GeoCatchingWebService/src/main/java/com/IutNiceGroupeB/app/App.java
+++ GeoCatchingWebService/src/main/java/com/IutNiceGroupeB/app/App.java
@@ -1,13 +1,30 @@
 package com.IutNiceGroupeB.app;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
 /**
  * Hello world!
  *
  */
-public class App 
+/*public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
     }
+}*/
+
+@Path("/hello")
+public class App {
+
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Bonjour " + msg;
+		return Response.status(200).entity(output).build();
+	}
 }
--- README.txt
+++ README.txt
@@ -16,9 +16,14 @@ Description of contents:
 For Android:
 	- Developpement en Android natif
 	- A ce jour, aucune installation
+	- Minimum API : 19
+
 For the web service:
 	- Pour build et run le projet : 
 		- Télécharger Maven à ce lien : https://maven.apache.org/download.cgi
 		- Installer Maven (Suivre ce tutoriel : https://maven.apache.org/install.html)
 		- Effectuer cette suite de commandes :
-			- 
+			- Build : mvn package
+			- Run : mvn jetty:run
+
+			- URL pour appeler le service : http://localhost:8080/GeoCatchingWebService/app/hello/{param}	
