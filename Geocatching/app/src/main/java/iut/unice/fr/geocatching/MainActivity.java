41d6c12c5181710340a7f1c594be56db0a1be461
De Robert Vincent <vincent.derobertt@gmail.com>
2017-01-05 17:51:15 +0100

    Update Affichage Connexion + Maps + event Onclick

--- Geocatching/app/src/main/java/iut/unice/fr/geocatching/MainActivity.java
+++ Geocatching/app/src/main/java/iut/unice/fr/geocatching/MainActivity.java
@@ -2,6 +2,8 @@ package iut.unice.fr.geocatching;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1 = (Button) findViewById(R.id.ConnectButton);
+        View.OnClickListener Connect = new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_maps);
+            }
+        };
+        b1.setOnClickListener(Connect);
     }
 
-    //HELLO WORLD
 }
--- Geocatching/app/src/main/java/iut/unice/fr/geocatching/MapsActivity.java
+++ Geocatching/app/src/main/java/iut/unice/fr/geocatching/MapsActivity.java
@@ -2,7 +2,7 @@ package iut.unice.fr.geocatching;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.util.Log;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,14 +10,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps2);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -35,12 +35,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap){
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
+        // Add a marker in S1ydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        GoogleMap.OnMapClickListener OnClickObject2 = new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng));
+            }
+        };
+        mMap.setOnMapClickListener(OnClickObject2);
     }
 }
