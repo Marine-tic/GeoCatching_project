6782837d6d73ee3b6b24f5c5e8b87f6f6cb3e9cc
ludivinefafournoux <Akeno@MacBook-Pro-de-Jonathan.local>
2017-01-06 16:59:45 +0100

    Ajout classe joueur + unitTest
    Ajout classe joueur + unitTest

--- Geocatching/app/src/main/java/iut/unice/fr/geocatching/Models/Joueur.java
+++ Geocatching/app/src/main/java/iut/unice/fr/geocatching/Models/Joueur.java
@@ -1,9 +1,49 @@
 package iut.unice.fr.geocatching.Models;
 
+import android.text.TextUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * Created by Lomenne on 06/01/2017.
+ * Created by Ludivine Fafournoux on 06/01/2017.
  */
 
 public class Joueur {
 
+    private String username;
+    private String email;
+
+
+    // Constructor
+    public Joueur(String username, String email) {
+        this.username = username;
+        this.email = email;
+        //verificationMail(email);
+    }
+
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+
+    // Setters
+    public void setUsername(String username) { this.username = username; }
+    public void setEmail(String email) { this.email = email; }
+
+
+    // Methods
+    @Override
+    public String toString() {
+        return "Username : " + username +
+                " Email : " + email ;
+    }
+
+    /*public Boolean verificationMail(CharSequence email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }*/
 }
---
+++ Geocatching/app/src/test/java/iut/unice/fr/geocatching/JoueurUnitTest.java
@@ -0,0 +1,62 @@
+package iut.unice.fr.geocatching;
+
+import android.text.TextUtils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import iut.unice.fr.geocatching.Models.Joueur;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+
+/**
+ * Created by Akeno on 06/01/2017.
+ */
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest(TextUtils.class)
+
+public class JoueurUnitTest {
+
+    private Joueur j;
+
+    @Before public void initialize() {
+        j = new Joueur("monPseudo", "monEmail@monemail.fr");}
+        /*public void setup() {
+            PowerMockito.mockStatic(TextUtils.class);
+            PowerMockito.when(TextUtils.isEmpty(any(CharSequence.class))).thenAnswer(new Answer<Boolean>() {
+                @Override
+                public Boolean answer(InvocationOnMock invocation) throws Throwable {
+                    CharSequence a = (CharSequence) invocation.getArguments()[0];
+                    return !(a != null && a.length() > 0);
+                }
+            });
+        }*/
+
+
+    @After public void reset(){
+        j = null;
+    }
+
+    @Test
+    public void testConstructeur() throws Exception {
+        assertEquals("monPseudo",j.getUsername());
+        assertEquals("monEmail@monemail.fr", j.getEmail());
+    }
+
+    @Test
+    public void testToString()
+    {
+
+        assertEquals("Username : monPseudo Email : monEmail@monemail.fr", j.toString());
+        assertNotEquals("Username : monNom Email : monEmail@monemail.fr", j.toString());
+    }
+
+
+
+}
