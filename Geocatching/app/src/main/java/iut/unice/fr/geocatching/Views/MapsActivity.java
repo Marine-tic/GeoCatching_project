fac96f8956c34594c71b88d084f0ae803dc57679
Marine Landraudie <marine.landraudie.tic@gmail.com>
2017-01-17 17:52:06 +0100

    - Nettoyage du code dans mapsActivity - Suppression du onClik

--- Geocatching/app/src/main/java/iut/unice/fr/geocatching/Views/MapsActivity.java
+++ Geocatching/app/src/main/java/iut/unice/fr/geocatching/Views/MapsActivity.java
@@ -2,24 +2,16 @@ package iut.unice.fr.geocatching.Views;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.util.ArrayList;
-
-import iut.unice.fr.geocatching.Helpers.Point;
 import iut.unice.fr.geocatching.Models.Joueur;
 import iut.unice.fr.geocatching.R;
-
-import static android.R.id.list;
-
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,25 +25,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_maps);
-
-        // Joueurs en dur en attendant le provider du Webservice
-       /* try {
-            Joueur joueur1 = new Joueur("Johnny", "johnny@gmail.com", new Point(43.616345d,7.072789d), true);
-            Joueur joueur2 = new Joueur("Paul", "Paul@gmail.com", new Point(43.620796d,7.070508d), true);
-            Joueur joueur3 = new Joueur("Germaine", "Germaine@gmail.com", new Point(43.620007d,7.065029d), true);
-            Joueur joueur4 = new Joueur("Michou", "Michou@gmail.com", new Point(43.616830d,7.076904d), true);
-            
-            // Création d'un eliste de joueurs pour récupérer les position
-            ArrayList<Joueur> playerPositionList = new ArrayList<>();
-            playerPositionList.add(joueur1);
-            playerPositionList.add(joueur2);
-            playerPositionList.add(joueur3);
-            playerPositionList.add(joueur4);
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -90,17 +63,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         CameraPosition target = CameraPosition.builder().target(iut).zoom(14).build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
         GoogleMap.OnMapLongClickListener OnClickObject2 = new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng));
-                //joueur1.getPosition().latitude
+
             public void onMapLongClick(LatLng latLng) {
                 compteur = compteur+1;
                 mMap.addMarker(new MarkerOptions().position(latLng).draggable(true).title("Pointeur "+(compteur)));
+                //joueur1.getPosition().latitude
             }
         };
         mMap.setOnMapLongClickListener(OnClickObject2);
-
 
 
         /**
