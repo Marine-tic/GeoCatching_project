03293c32b5ccc534b920157a444791e3f586346d
Mennella <loic.mennella@gmail.com>
2017-01-22 02:31:54 +0100

    LPDB-38 - Correction du bug de création d'une zone au clique sur leterrain

--- GeoCatching/app/src/main/java/iut/unice/fr/geocatching/Views/MapsActivity.java
+++ GeoCatching/app/src/main/java/iut/unice/fr/geocatching/Views/MapsActivity.java
@@ -7,7 +7,6 @@ import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -250,7 +249,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     listMarker.clear();
                 }
 
-                else {
+                else if(polygon.getStrokeColor() == Color.MAGENTA && polygon.getFillColor() == Color.argb(100, 100, 100, 100)) {
                     polygon.remove();
                     polygon = mMap.addPolygon(new PolygonOptions()
                             .addAll(polygon.getPoints())
