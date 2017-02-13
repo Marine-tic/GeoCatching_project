11dab392cfc3644d6dc8e1edb4730f1fe06a373c
Marine Landraudie <marine.landraudie.tic@gmail.com>
2017-02-13 01:07:37 +0100

    LPDB-55 - Création de l'IA du jeu Création de l'IA avec la séquence de l'ordinateur (exemple à suivre pour le joueur). Génération de séquences aléatoires Génération de boutons Activation des boutons sur une séquence aléatoire (complément de LPDB 56 - Création Animation du jeu) Réalisé par Ludivine Fafournoux, Laura Daufeld et Marine Landraudie

--- GeoCatching/app/src/main/java/iut/unice/fr/geocatching/Views/GameActivity.java
+++ GeoCatching/app/src/main/java/iut/unice/fr/geocatching/Views/GameActivity.java
@@ -1,15 +1,194 @@
 package iut.unice.fr.geocatching.Views;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import iut.unice.fr.geocatching.Models.Figure;
+import iut.unice.fr.geocatching.Models.FigureList;
 import iut.unice.fr.geocatching.R;
 
 public class GameActivity extends Activity {
+
+
+    private int sequenceFigureMaxSize;
+    private FigureList figureList;
+    private ImageButton triangleImageButton;
+    private ImageButton ovalImageButton;
+    private ImageButton squareImageButton;
+    private ImageButton polygonImageButton;
+
+    private Figure triangle;
+    private Figure oval;
+    private Figure square;
+    private Figure polygon;
+    private List<Figure> sequenceFigureComputer;
+
+    
+    public GameActivity(){
+        // Contains the list of all possible figures
+        figureList = new FigureList();
+
+        // init figure object from the list
+        triangle = figureList.getFigureFromNameInFigureList("Triangle");
+        oval = figureList.getFigureFromNameInFigureList("Oval");
+        square = figureList.getFigureFromNameInFigureList("Square");
+        polygon = figureList.getFigureFromNameInFigureList("Polygon");
+
+        // init figure sequence for computer and for the player
+        sequenceFigureComputer = new ArrayList<>();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+               initView();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Animation of the computer are played
+        playAnimation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }
+
+
+    /**
+     * Initialization of the figure images button with the view
+     * Initialization of the pictures for the figure in the image button
+     */
+    private void initImageButtons() {
+        // Init image button
+        ovalImageButton = (ImageButton) findViewById(oval.getImageButtonId());
+        triangleImageButton = (ImageButton) findViewById(triangle.getImageButtonId());
+        polygonImageButton = (ImageButton) findViewById(polygon.getImageButtonId());
+        squareImageButton = (ImageButton) findViewById(square.getImageButtonId());
+
+        // Picture attribution of the figure for the image button
+        ovalImageButton.setImageResource(oval.getSmallPictureId());
+        triangleImageButton.setImageResource(triangle.getSmallPictureId());
+        polygonImageButton.setImageResource(polygon.getSmallPictureId());
+        squareImageButton.setImageResource(square.getSmallPictureId());
+    }
+
+    private void initView() {
+        initImageButtons();
+        updateListFigure();
+        initSequenceFigureComputer();
+
+    }
+
+
+
+
+
+    // Create the random figure sequence of the computer
+    private void initSequenceFigureComputer() {
+        int minimum = 0;
+        int maximum = figureList.size() - 1;
+        for(int i = 0; i < sequenceFigureMaxSize; ++i){
+            // The random is slightly modified to avoid the same sequence of 1 figure only
+            if(i == 3 % 2){
+                sequenceFigureComputer.add(figureList.getFigureList().get(3));
+            } else if (i == 2 % 3){
+                sequenceFigureComputer.add(figureList.getFigureList().get(2));
+            }
+            else {
+                int randomNum = minimum + (int)(Math.random() * maximum);
+                sequenceFigureComputer.add(figureList.getFigureList().get(randomNum));
+            }
+        }
+    }
+
+    /**
+     * This function will enable the figure's image button to able the player to play
+     */
+    private void enableAllButton() {
+        ovalImageButton.setEnabled(true);
+        polygonImageButton.setEnabled(true);
+        squareImageButton.setEnabled(true);
+        triangleImageButton.setEnabled(true);
+    }
+
+    /**
+     * This function will disable the figure's image button to able the computer to play without being disturbed by the player
+     */
+    private void disableAllButton() {
+        ovalImageButton.setEnabled(false);
+        polygonImageButton.setEnabled(false);
+        squareImageButton.setEnabled(false);
+        triangleImageButton.setEnabled(false);
+    }
+
+
+    /**
+     * This function will animate each button after it's clicked
+     */
+    private void playAnimation() {
+        for (int i = 0; i < sequenceFigureComputer.size(); i++) {
+
+            final int finalI = i;
+            final Context context = this;
+            new Handler().postDelayed(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    disableAllButton();
+                    sequenceFigureComputer.get(finalI).animateFigure(context,
+                            (ImageButton)findViewById(sequenceFigureComputer.get(finalI).getImageButtonId()));
+                    if(finalI == sequenceFigureComputer.size() - 1){
+                        enableAllButton();
+                    }
+                }
+            }, 1000 * i);
+            if(i == sequenceFigureComputer.size() - 1){
+                enableAllButton();
+            }
+        }
+
+    }
+
+    /**
+     * The list of figure is updated with the last version of figure initialised during the game
+     * To avoid inconsistencies and problems
+     */
+    private void updateListFigure() {
+        List<Figure> tempListFigure = new ArrayList<>();
+        tempListFigure.add(triangle);
+        tempListFigure.add(oval);
+        tempListFigure.add(polygon);
+        tempListFigure.add(square);
+        figureList.setFigureList(tempListFigure);
+    }
+
+    @Override
+    protected void onPause () {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
